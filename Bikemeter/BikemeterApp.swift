911a729037fe952a7a799c0c61bdbce2a3c91a54
Andrius Shiaulis <shiaulis@gmail.com>
2022-06-19 19:23:37 +0300

    Initial commit

---
+++ Bikemeter/BikemeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikemeterApp.swift
+//  Bikemeter
+//
+//  Created by Andrius Shiaulis on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BikemeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
